9fe63d0f5a816360178e23fe4aab7032ed893563
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-28 17:29:45 +0200

    Brew formula update for go-gh-actions version v1.2.4

--- Formula/go-gh-actions.rb
+++ Formula/go-gh-actions.rb
@@ -11,22 +11,22 @@ class GoGhActions < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/lgdd/go-gh-actions/releases/download/v1.2.4/go-gh-actions_1.2.4_darwin_amd64.tar.gz"
-      sha256 "a36c62543d773fc8c4ef692f57a30189b1f82fcec63c3347ed74e1555a1bc422"
+      sha256 "92eed42001bf6d811a1a9ee9b644f4011ccd31cfb44e966e892e40259d04380e"
     end
     if Hardware::CPU.arm?
       url "https://github.com/lgdd/go-gh-actions/releases/download/v1.2.4/go-gh-actions_1.2.4_darwin_arm64.tar.gz"
-      sha256 "0c1038db3bbe9342ca64658da53f698336611c69af88e894fe7139528883e1b6"
+      sha256 "b80999d3580feb08274b5c9467347aeda6bf8eb62bdca8d7d0321057c67760c5"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/lgdd/go-gh-actions/releases/download/v1.2.4/go-gh-actions_1.2.4_linux_amd64.tar.gz"
-      sha256 "d3a9201ad39d33d74d15fb7af97d3af37b5081d2548a5840617315ae59def137"
+      sha256 "892480fcf176ccbba2b1dea355c3524d621134b65e016f2976dff98af3c94c7d"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/lgdd/go-gh-actions/releases/download/v1.2.4/go-gh-actions_1.2.4_linux_arm64.tar.gz"
-      sha256 "c8a3b5885f6b08827d03fcf896047b811423f796fef2905c4853ca269d41fbe0"
+      sha256 "18d3ae1578d20eb791dd187fc5e84009a07909eeba798627c1c9f8f991722349"
     end
   end
 
